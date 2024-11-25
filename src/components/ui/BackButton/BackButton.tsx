292f26ead408fe372513482d9e36deb3d69d49ec
jymaeng1234 <102141309+jymaeng1234@users.noreply.github.com>
2024-11-25 15:37:42 +0900

    [Feat] QFEED-29 공통 컴포넌트 구현 (#7)
    * feat: qfeed-29 삭제 버튼 추가
    
    * fix: qfeed-29 삭제버튼 폴더 이동
    
    * feat: qfeed-29 수정 버튼 추가
    
    * feat: qfeed-29 뒤로가기 버튼 추가

---
+++ src/components/ui/BackButton/BackButton.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+import { Button as ChakraButton } from '@chakra-ui/react';
+import { IoMdArrowRoundBack } from "react-icons/io";
+
+type BackButtonProps = {
+  onClick?: () => void;
+  width?: string | number;
+  height?: string | number;
+};
+
+const StyledButton = styled(ChakraButton)<{ width?: string | number; height?: string | number }>`
+  cursor: pointer;
+  border: none;
+  background: transparent;
+  width: ${props => props.width || 'auto'};
+  height: ${props => props.height || 'auto'};
+
+  svg {
+    width: 70%;
+    height: 70%;
+    transition: all 0.3s ease-in-out;
+  }
+
+  &:hover {
+    svg {
+      color: gray;
+      transform: translateX(-5px);
+    }
+  }
+
+`;
+
+export const BackButton = ({ onClick, width='2.5rem', height='2.5rem'  }: BackButtonProps) => {
+  return (
+      <StyledButton
+        variant="ghost"
+        size="md"
+        onClick={onClick}
+        width={width}
+        height={height}
+      >
+        <IoMdArrowRoundBack />
+      </StyledButton>
+  );
+};
---
+++ src/components/ui/EditButton/EditButton.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+import { Button as ChakraButton, Tooltip } from '@chakra-ui/react';
+import { MdEdit } from "react-icons/md";
+
+type EditButtonProps = {
+  onClick?: () => void;
+  width?: string | number;
+  height?: string | number;
+};
+
+const StyledButton = styled(ChakraButton)<{ width?: string | number; height?: string | number }>`
+  cursor: pointer;
+  border: none;
+  background: transparent;
+  width: ${props => props.width || 'auto'};
+  height: ${props => props.height || 'auto'};
+
+  svg {
+    width: 70%;
+    height: 70%;
+  }
+
+  &:hover {
+    svg {
+      color: gray;
+    }
+  }
+
+`;
+
+export const EditButton = ({ onClick, width='2.5rem', height='2.5rem'  }: EditButtonProps) => {
+  return (
+    <Tooltip label="수정하기" placement="top" hasArrow >
+      <StyledButton
+        variant="ghost"
+        size="md"
+        onClick={onClick}
+        width={width}
+        height={height}
+      >
+        <MdEdit />
+      </StyledButton>
+    </Tooltip>
+  );
+};
