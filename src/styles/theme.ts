bacc41e1c90388e1615b774e6bf7aad8984cc941
se0kcess <kimminsuk051@gmail.com>
2024-11-26 01:29:28 +0000

    style: 스타일가이드 적용

---
+++ src/styles/theme.ts
@@ -0,0 +1,74 @@
+const theme = {
+  colors: {
+    primary: '#9D6F70',
+    sub: '#F0E8DC',
+    red: '#F75454',
+    blue: '#59B3F8',
+    yellow: '#FCF4D0',
+    textYellow: '#C1A830',
+    background: '#F3EBE0',
+    white: '#ffffff',
+    black: '#121212',
+    gray: {
+      100: '#EFEDED',
+      200: '#D3CDCD',
+      300: '#BAB0B1',
+      400: '#A09596',
+      500: '#877D7D',
+      600: '#685F60',
+    },
+  },
+  typography: {
+    fontFamily: 'Noto Sans KR',
+    header1: {
+      fontFamily: 'GyeonggiBatang',
+      size: '24px',
+      lineHeight: 1.4,
+      weight: 700,
+    },
+    header2: {
+      fontFamily: 'GyeonggiBatang',
+      size: '20px',
+      lineHeight: 1.4,
+      weight: 700,
+    },
+    title1: {
+      size: '20px',
+      lineHeight: 1.4,
+      weight: 600,
+    },
+    title2: {
+      size: '18px',
+      lineHeight: 1.4,
+      weight: 500,
+    },
+    body1: {
+      size: '16px',
+      lineHeight: 1.4,
+      weight: 400,
+    },
+    body2: {
+      size: '14px',
+      lineHeight: 1.4,
+      weight: 400,
+    },
+    body3: {
+      size: '12px',
+      lineHeight: 1.4,
+      weight: 400,
+    },
+    buttonText: {
+      size: '16px',
+      lineHeight: 1.4,
+      weight: 400,
+    },
+    weights: {
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+    },
+  },
+};
+
+export default theme;
