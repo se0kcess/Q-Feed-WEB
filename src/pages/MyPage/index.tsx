e63ff7f5dd1dc1fc323ef3681c77764f82e9a7e1
ahnjongin <goodtime1998@naver.com>
2024-11-29 16:41:57 +0900

    fix: qfeed-69 index파일 수정

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,10 +1,3 @@
-import FollowerFollowingPage from '@/pages/follower';
-
 export default function MyPage() {
-  return (
-    <div>
-      ai code review test
-      <FollowerFollowingPage />
-    </div>
-  );
+  return <div>ai code review test</div>;
 }
