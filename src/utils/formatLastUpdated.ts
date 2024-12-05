863678281a0174b6ddc4cd566c777e053f570873
se0kcess <kimminsuk051@gmail.com>
2024-12-05 00:33:45 +0000

    fix: 게시글 작성 시간 렌더링 함수 글자 수정

--- src/utils/formatLastUpdated.ts
+++ src/utils/formatLastUpdated.ts
@@ -6,8 +6,8 @@ export const formatLastUpdated = (dateString: string) => {
   if (diffInMinutes < 60) {
     return '방금 전 게시';
   } else if (diffInMinutes < 1440) {
-    return `${Math.floor(diffInMinutes / 60)}시간 전 대화`;
+    return `${Math.floor(diffInMinutes / 60)}시간 전 게시`;
   } else {
-    return `${Math.floor(diffInMinutes / 1440)}일 전 대화`;
+    return `${Math.floor(diffInMinutes / 1440)}일 전 게시`;
   }
 };
