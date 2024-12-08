ba4907b0b76ca3f3385a4d7b2e9d81817ab58dc3
MinSeok Kim <kimminsuk051@gmail.com>
2024-12-08 21:37:58 +0900

    refactor: login api 응답 구조 수정(userid)

--- src/pages/Login/hooks/useLogin.ts
+++ src/pages/Login/hooks/useLogin.ts
@@ -4,14 +4,18 @@ import { fetchLogin } from '@/pages/Login/api/auth';
 import { setCookie } from '@/utils/cookies';
 import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from '@/constants/token';
 import { AUTH_KEYS } from '@/api/queryKeys';
+import { useUserStore } from '@/store/userStore';
 
 export const useLogin = () => {
+  const { setUserId } = useUserStore();
+
   return useMutation<LoginResponse, Error, LoginRequest>({
     mutationKey: [AUTH_KEYS.ROOT, AUTH_KEYS.ACTIONS.LOGIN],
     mutationFn: (loginData: LoginRequest) => fetchLogin(loginData),
     onSuccess: (data: LoginResponse) => {
       setCookie(ACCESS_TOKEN_KEY, data.accessToken);
       setCookie(REFRESH_TOKEN_KEY, data.refreshToken);
+      setUserId(data.userId);
     },
   });
 };
