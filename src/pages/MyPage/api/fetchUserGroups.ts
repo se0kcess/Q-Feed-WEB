0e841cdd9b7e6a318e6de43b629ac98165ac154b
Sunami97 <sunami7191@daum.net>
2024-12-31 22:35:57 +0900

    fix: 내가 참여한 그룹 목록 조회 api 수정

--- src/pages/MyPage/api/fetchUserGroups.ts
+++ src/pages/MyPage/api/fetchUserGroups.ts
@@ -2,7 +2,7 @@ import { apiClient } from '@/api/fetch';
 import { Group } from '@/pages/MyPage/types/group';
 
 export const fetchUserGroups = async (): Promise<Group[]> => {
-  const response = await apiClient.get<Group[]>('/groups/user/');
+  const response = await apiClient.get<Group[]>('/groups/user');
 
   if (!response.data) {
     throw new Error('No data received from the server.');
