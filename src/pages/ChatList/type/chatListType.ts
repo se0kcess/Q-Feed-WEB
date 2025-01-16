b599b62a47aa92a4f1502a525dca3524fce69969
ahnjongin <80513276+ahnjongin@users.noreply.github.com>
2025-01-16 22:54:02 +0900

    [Feat] QFEED-163 채팅방 기능 추가 (#96)
    * feat: qfeed-163 실시간 알림 세팅(토큰 전송 api 연결)
    
    * feat: qfeed-163 알림 처리, 환경변수 처리
    
    * fix: qfeed-163 firebase 초기화
    
    * fix: qfeed-163 chat userid 동적으로 변경
    
    * feat: qfeed-163 채팅방 만들기 api 호출, 채팅 방식  변경

--- src/pages/ChatList/type/chatListType.ts
+++ src/pages/ChatList/type/chatListType.ts
@@ -2,6 +2,7 @@ export interface ChatData {
   chatRoomId: string; // 채팅방 ID
   otherUserProfile?: string; // 프로필 이미지 URL
   otherUserNickname: string; // 사용자 이름
+  otherUserId: string; //상대 userid
   lastMessageContent: string; // 마지막 메시지
   lastMessageCreatedAt: string; // 메시지 시간
   unreadMessageCount?: number; // 읽지 않은 메시지 수
--- src/pages/Profile/api/fetchFollow.ts
+++ src/pages/Profile/api/fetchFollow.ts
@@ -1,13 +1,16 @@
 // fetchFollows.ts
 import { apiClient } from '@/api/fetch';
 import { FollowStatusResponse, FollowListResponse } from '@/pages/Profile/types/follow';
+import { ChatData } from '@/pages/ChatList/type/chatListType';
 
 // 팔로우 상태 확인
-export const fetchFollowStatus = async (followerId: string, followeeId: string): Promise<FollowStatusResponse> => {
-  const response = await apiClient.get<FollowStatusResponse>(
-    '/follows/check',
-    { params: { followerId, followeeId } }
-  );
+export const fetchFollowStatus = async (
+  followerId: string,
+  followeeId: string
+): Promise<FollowStatusResponse> => {
+  const response = await apiClient.get<FollowStatusResponse>('/follows/check', {
+    params: { followerId, followeeId },
+  });
 
   if (!response.data) {
     throw new Error('No data received from API');
@@ -76,4 +79,33 @@ export const fetchFollowerList = async (
   }
 
   return response.data;
+};
+
+export const chatAPI = {
+  // 채팅방 목록 가져오기
+  getChatList: () => apiClient.get<ChatData[]>('/chats'),
+
+  // 채팅방 생성
+  createChatRoom: (userId: string, targetUserId: string) =>
+    apiClient.post('/chats/create', { userId, targetUserId }), // userId와 상대 userId를 이용한 채팅방 생성
+};
+
+export const fetchChatList = async (): Promise<ChatData[]> => {
+  try {
+    const response = await chatAPI.getChatList();
+    console.log('응답 데이터: ', response.data); // 응답 확인
+    return response.data || [];
+  } catch (error) {
+    console.error('API 요청 중 오류 발생: ', error); // 에러 확인
+    return [];
+  }
+};
+
+export const createChatRoom = async (userId: string, targetUserId: string): Promise<void> => {
+  try {
+    const response = await chatAPI.createChatRoom(userId, targetUserId);
+    console.log('채팅방 생성 완료:', response.data);
+  } catch (error) {
+    console.error('채팅방 생성 실패:', error);
+  }
 };
